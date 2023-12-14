ea3c86e7ed1342d08a10ad8fe99a98a9f22af20d
Giovanni Torelli <giovanni.walter@outlook.com>
2023-12-14 12:56:01 -0300

    Tested Conta Class on Menu

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -1,5 +1,6 @@
 package conta;
 
+import conta.model.Conta;
 import conta.util.Colors;
 
 import java.util.Scanner;
@@ -7,6 +8,13 @@ import java.util.Scanner;
 public class Menu {
 
     public static void main(String[] args) {
+
+        Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+        c1.visualizar();
+        c1.sacar(12000.0f);
+        c1.visualizar();
+        c1.depositar(5000.0f);
+        c1.visualizar();
 
         Scanner input = new Scanner(System.in);
 
